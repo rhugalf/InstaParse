362bcd5ca325729dd3de0e1a9ddc4e10f07cfa72
Hugo <hugo@Hugos-Mac.local>
2014-10-29 15:46:19 -0700

    Initial Commit

---
+++ InstaParse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaParse
+//
+//  Created by Hugo on 10/29/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaParse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaParse
+//
+//  Created by Hugo on 10/29/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
